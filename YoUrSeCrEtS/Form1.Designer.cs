b3bdd0f8a662502864ab9ac9943ae1ad1b4eac5f
Exsper <starlightexsper@foxmail.com>
2021-01-27 23:10:49 +0800

    添加项目文件。

---
+++ YoUrSeCrEtS/Form1.Designer.cs
@@ -0,0 +1,132 @@
+
+namespace YoUrSeCrEtS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LeftTextBox = new System.Windows.Forms.TextBox();
+            this.RightTextBox = new System.Windows.Forms.TextBox();
+            this.LeftToRightButton = new System.Windows.Forms.Button();
+            this.RightToLeftButton = new System.Windows.Forms.Button();
+            this.LeftComboBox = new System.Windows.Forms.ComboBox();
+            this.RightComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // LeftTextBox
+            // 
+            this.LeftTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftTextBox.Location = new System.Drawing.Point(12, 41);
+            this.LeftTextBox.Multiline = true;
+            this.LeftTextBox.Name = "LeftTextBox";
+            this.LeftTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LeftTextBox.Size = new System.Drawing.Size(400, 383);
+            this.LeftTextBox.TabIndex = 0;
+            this.LeftTextBox.Text = "偷偷告诉你，其实我喜欢你已经很久了";
+            // 
+            // RightTextBox
+            // 
+            this.RightTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightTextBox.Location = new System.Drawing.Point(518, 41);
+            this.RightTextBox.Multiline = true;
+            this.RightTextBox.Name = "RightTextBox";
+            this.RightTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.RightTextBox.Size = new System.Drawing.Size(400, 383);
+            this.RightTextBox.TabIndex = 1;
+            this.RightTextBox.TextChanged += new System.EventHandler(this.RightTextBox_TextChanged);
+            // 
+            // LeftToRightButton
+            // 
+            this.LeftToRightButton.Location = new System.Drawing.Point(418, 41);
+            this.LeftToRightButton.Name = "LeftToRightButton";
+            this.LeftToRightButton.Size = new System.Drawing.Size(94, 138);
+            this.LeftToRightButton.TabIndex = 4;
+            this.LeftToRightButton.Text = "->";
+            this.LeftToRightButton.UseVisualStyleBackColor = true;
+            this.LeftToRightButton.Click += new System.EventHandler(this.LeftToRightButton_Click);
+            // 
+            // RightToLeftButton
+            // 
+            this.RightToLeftButton.Location = new System.Drawing.Point(418, 286);
+            this.RightToLeftButton.Name = "RightToLeftButton";
+            this.RightToLeftButton.Size = new System.Drawing.Size(94, 138);
+            this.RightToLeftButton.TabIndex = 5;
+            this.RightToLeftButton.Text = "<-";
+            this.RightToLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // LeftComboBox
+            // 
+            this.LeftComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LeftComboBox.FormattingEnabled = true;
+            this.LeftComboBox.Location = new System.Drawing.Point(12, 12);
+            this.LeftComboBox.Name = "LeftComboBox";
+            this.LeftComboBox.Size = new System.Drawing.Size(400, 20);
+            this.LeftComboBox.TabIndex = 6;
+            // 
+            // RightComboBox
+            // 
+            this.RightComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.RightComboBox.FormattingEnabled = true;
+            this.RightComboBox.Location = new System.Drawing.Point(518, 12);
+            this.RightComboBox.Name = "RightComboBox";
+            this.RightComboBox.Size = new System.Drawing.Size(400, 20);
+            this.RightComboBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 436);
+            this.Controls.Add(this.RightComboBox);
+            this.Controls.Add(this.LeftComboBox);
+            this.Controls.Add(this.RightToLeftButton);
+            this.Controls.Add(this.LeftToRightButton);
+            this.Controls.Add(this.RightTextBox);
+            this.Controls.Add(this.LeftTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "YoUrSeCrEtS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LeftTextBox;
+        private System.Windows.Forms.TextBox RightTextBox;
+        private System.Windows.Forms.Button LeftToRightButton;
+        private System.Windows.Forms.Button RightToLeftButton;
+        private System.Windows.Forms.ComboBox LeftComboBox;
+        private System.Windows.Forms.ComboBox RightComboBox;
+    }
+}
+
---
+++ YoUrSeCrEtS/Form1.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoUrSeCrEtS
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> encodings = new Dictionary<string, string>();
+
+        private void DicInit()
+        {
+            encodings.Add("简体中文 (GB2312)", "gb2312");
+            encodings.Add("简体中文 (GB18030)", "GB18030");
+            encodings.Add("繁体中文 (Big5)", "big5");
+            encodings.Add("日语 (Shift-JIS)", "shift_jis");
+            encodings.Add("Unicode (UTF-8)", "utf-8");
+            encodings.Add("Unicode", "utf-16");
+            encodings.Add("Unicode (Big-Endian)", "UnicodeFFFE");
+            encodings.Add("Unicode (UTF-7)", "utf-7");
+            encodings.Add("Unicode (UTF-32)", "utf-32");
+            encodings.Add("Unicode (UTF-32 Big-Endian)", "utf-32BE");
+        }
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            DicInit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var ec in encodings)
+            {
+                LeftComboBox.Items.Add(ec.Key);
+                RightComboBox.Items.Add(ec.Key);
+            }
+            LeftComboBox.SelectedItem = "Unicode (UTF-8)";
+            RightComboBox.SelectedIndex = 0;
+        }
+
+        private string Translate(string words, string fromCoding, string toCoding)
+        {
+            Encoding from = Encoding.GetEncoding(fromCoding);
+            byte[] fromBytes = from.GetBytes(words);
+            Encoding to = Encoding.GetEncoding(toCoding);
+            string toString = to.GetString(fromBytes);
+            return toString;
+        }
+
+        private void LeftToRightButton_Click(object sender, EventArgs e)
+        {
+            string input = LeftTextBox.Text;
+            if (input == "") return;
+            string fromCoding = encodings[LeftComboBox.Text];
+            string toCoding = encodings[RightComboBox.Text];
+            string output = Translate(input, fromCoding, toCoding);
+            RightTextBox.Text = output;
+        }
+
+        private void RightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string input = RightTextBox.Text;
+            if (input == "") return;
+            string fromCoding = encodings[RightComboBox.Text];
+            string toCoding = encodings[LeftComboBox.Text];
+            string output = Translate(input, fromCoding, toCoding);
+            LeftComboBox.Text = output;
+        }
+    }
+}
